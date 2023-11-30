8bc61f3aa4e52e7ce65514b3ea1c3037ef97e66f
jbkim1218 <84824637+jbkim1218@users.noreply.github.com>
2023-11-30 11:49:53 +0900

    Add files via upload
    화면 초기 틀

---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
+import { Image } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = require('./assets/home.png');
+            } else if (route.name === 'Search') {
+              iconName = require('./assets/search.png');
+            }
+       
+            return <Image source={iconName} style={{ width: size, height: size }} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
